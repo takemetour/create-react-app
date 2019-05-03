b60d31b2b0e4fcb62b290745b31787116855c366
Tananan Tangthanachaikul <microbenz.prob@gmail.com>
2019-05-03 14:32:41 +0700

    Add bundle analyzer

--- packages/react-scripts/config/webpack.config.js
+++ packages/react-scripts/config/webpack.config.js
@@ -30,6 +30,8 @@ const paths = require('./paths');
 const getClientEnvironment = require('./env');
 const ModuleNotFoundPlugin = require('react-dev-utils/ModuleNotFoundPlugin');
 const ForkTsCheckerWebpackPlugin = require('react-dev-utils/ForkTsCheckerWebpackPlugin');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer')
+  .BundleAnalyzerPlugin;
 const typescriptFormatter = require('react-dev-utils/typescriptFormatter');
 // @remove-on-eject-begin
 const getCacheIdentifier = require('react-dev-utils/getCacheIdentifier');
@@ -633,6 +635,10 @@ module.exports = function(webpackEnv) {
           silent: true,
           // The formatter is invoked directly in WebpackDevServerUtils during development
           formatter: isEnvProduction ? typescriptFormatter : undefined,
+        }),
+      isEnvDevelopment &&
+        new BundleAnalyzerPlugin({
+          analyzerMode: 'server',
         }),
     ].filter(Boolean),
     // Some libraries import Node modules but don't use them in the browser.
